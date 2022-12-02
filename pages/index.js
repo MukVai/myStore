7a25361f7265ac87dde9a2d8de04fb7cd6f3d41a
Mukesh Vaishnav <mukeshvaishnav018@gmail.com>
2022-12-02 19:14:02 +0530

    Created Components and edited index.js and _app.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from 'react';
+
+import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = () => {
   return (
-    <div>
-      Herobanner 
-      <div>
+    <div >
+      <HeroBanner />
+      <div className="products-heading">
         <h2> Best Selling Products </h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aliquid vitae, nemo repellat impedit esse quasi at laboriosam laudantium cum ducimus? Dolorum, dolor?</p>
       </div>
 
-      <div>{
-        ['Product 1', 'Product 2'].map((product)=>product)
+      <div className="products-container">{
+        ['Product 1', 'Product 2'].map((product) => product)
       }
       </div>
 
-      Footer 
+      <FooterBanner />
     </div>
   )
 }
