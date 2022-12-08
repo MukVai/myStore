86fc6cf07779701eb693cf78321e00086ece2ac1
Mukesh Vaishnav <mukeshvaishnav018@gmail.com>
2022-12-08 13:28:55 +0530

    Completed FooterBanner, HeroBanner and Product Components

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,18 @@ import { Product, FooterBanner, HeroBanner } from '../components';
 const Home = ({ products, bannerData }) => {
   return (
     <div >
-      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />    {/* check if banner-len!=0 and if so, return 1st element */}
       <div className="products-heading">
         <h2> Best Selling Products </h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aliquid vitae, nemo repellat impedit esse quasi at laboriosam laudantium cum ducimus? Dolorum, dolor?</p>
       </div>
 
       <div className="products-container">{
-        products?.map((product) => product.name)
+        products?.map((product) => <Product key={product._id} product={product} />)
       }
       </div>
 
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </div>
   )
 }
