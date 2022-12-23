221fea4a0f1494a3f2aac11179669bb67fa8d2f3
Mukesh Vaishnav <mukeshvaishnav018@gmail.com>
2022-12-23 19:14:10 +0530

    Added 'Add-to-cart' option

--- context/StateContext.js
+++ context/StateContext.js
@@ -1,11 +1,12 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { toast } from 'react-hot-toast';
+import { Product } from "../components";
 
 const Context = createContext();
 
 export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false);
-    const [cartItems, setCartItems] = useState();
+    const [cartItems, setCartItems] = useState([]);
     const [totalPrice, setTotalPrice] = useState();
     const [totalQuantities, setTotalQuantities] = useState();
     const [qty, setQty] = useState(1);
@@ -21,14 +22,38 @@ export const StateContext = ({ children }) => {
         });
     }
 
+    const onAdd = (product, quantity) => {
+        const checkIfItemIsAlreadyInCart = cartItems.find((item) => item._id === product._id);
+        //loop on all items in the cart and return where id is equal
+        setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+        if (checkIfItemIsAlreadyInCart) {
+
+            const updatedCartItems = cartItems.map((cartProduct) => {
+                if (cartProduct._id === product._id) return {
+                    ...cartProduct,
+                    quantity: cartProduct.quantity + quantity
+                }
+            })
+
+            setCartItems(updatedCartItems);
+        } else {
+            product.quantity = quantity;
+            setCartItems([...cartItems, { ...product }]);
+        }
+
+        toast.success(`${qty} ${product.name} added to the cart.`)
+    }
+
     return (
         < Context.Provider value={{
             showCart, cartItems, totalPrice, totalQuantities, qty,
-            incQty, decQty
+            incQty, decQty, onAdd,
         }} >
             {children}
         </Context.Provider >
     )
 }
 
-export const useStateContext = () => useContext(Context);
+export const useStateContext = () => useContext(Context); 
