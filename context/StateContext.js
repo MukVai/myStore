28f1791cc3a123678e7ab68cfefc252d627d53c7
Mukesh Vaishnav <mukeshvaishnav018@gmail.com>
2023-01-02 17:08:13 +0530

    Cart Logic Completed

--- context/StateContext.js
+++ context/StateContext.js
@@ -7,9 +7,12 @@ const Context = createContext();
 export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false);
     const [cartItems, setCartItems] = useState([]);
-    const [totalPrice, setTotalPrice] = useState();
-    const [totalQuantities, setTotalQuantities] = useState();
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
+
+    let foundProduct;
+    let index;
 
     const incQty = () => {
         setQty((prevQty) => prevQty + 1);
@@ -46,10 +49,42 @@ export const StateContext = ({ children }) => {
         toast.success(`${qty} ${product.name} added to the cart.`)
     }
 
+    const onRemove = (product) => {
+        foundProduct = cartItems.find((item) => item._id === product._id)
+        const newCartItems = cartItems.filter((item) => item._id !== product._id)
+
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price * foundProduct.quantity);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - foundProduct.quantity);
+        setCartItems(newCartItems);
+
+    }
+
+    const toggleCartItemQuantity = (id, value) => {
+        foundProduct = cartItems.find((item) => item._id === id)
+        index = cartItems.findIndex((product) => product._id === id)
+        const newCartItems = cartItems.filter((item) => item._id !== id);
+        // remove '1' item from 'index' position in the array - use splice - but splice is a mutative method and we must not mutate a state in react, thus we use filter which is non-mutative, and thus remove the item we are updating out of the list so as to re-add it later while updateing the state by setCartItems without mutating the state.
+
+        if (value === 'inc') {
+            setCartItems([...newCartItems.slice(0, index), { ...foundProduct, quantity: foundProduct.quantity + 1 }, ...newCartItems.slice(index)])
+            // slice is used to update the product and keep it at the same index as it was before
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+            setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1)
+        } else if (value === 'dec') {
+            if (foundProduct.quantity > 1) {
+                // updating the cartItems by removing 1 more quantity to a product
+                // setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 }]);
+                setCartItems([...newCartItems.slice(0, index), { ...foundProduct, quantity: foundProduct.quantity - 1 }, ...newCartItems.slice(index)])
+                setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+                setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1)
+            }
+        }
+    }
+
     return (
         < Context.Provider value={{
-            showCart, cartItems, totalPrice, totalQuantities, qty,
-            incQty, decQty, onAdd,
+            showCart, setShowCart, cartItems, totalPrice, totalQuantities, qty,
+            incQty, decQty, onAdd, toggleCartItemQuantity, onRemove
         }} >
             {children}
         </Context.Provider >
